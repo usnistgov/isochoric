4560ad188bac83da3c0c889643c018c7843fa059
Ian Bell <ian.bell@nist.gov>
2021-07-28 08:11:36 -0400

    Bump to version 0.9, try to make wheels

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='VLEIsoTracer',
-    version='0.0.0',
+    version='0.9.0',
     author='Ian Bell',
     author_email='ian.bell@nist.gov',
     description='Tracer for isolines of VLE phase envelopes',
